e4c653e4ff53e47cf2b4ecd89458f5f15046647e
kyonion <mschlaipfer@gmx.net>
2015-03-13 15:09:48 +0100

    getdata012
    tidying stage

--- run_analysis.R
+++ run_analysis.R
@@ -7,12 +7,15 @@ train_subject <- read.table("getdata/UCI HAR Dataset/train/subject_train.txt")
 col_names <- make.names(read.table("getdata/UCI HAR Dataset/features.txt")[[2]],unique=TRUE)
 activity_labels <- read.table("getdata/UCI HAR Dataset/activity_labels.txt")
 
+test_activity <- left_join(test_activity,activity_labels,by="V1")
+train_activity <- left_join(train_activity,activity_labels,by="V1")
+
 colnames(test_data) <- col_names
 colnames(train_data) <- col_names
 colnames(test_subject) <- "subject"
 colnames(train_subject) <- "subject"
-colnames(test_activity) <- "activity"
-colnames(train_activity) <- "activity"
+colnames(test_activity)[2] <- "activity"
+colnames(train_activity)[2] <- "activity"
 
 test <- tbl_df(bind_cols(test_subject,test_activity,test_data))
 train <- tbl_df(bind_cols(train_subject,train_activity,train_data))
@@ -20,6 +23,5 @@ train <- tbl_df(bind_cols(train_subject,train_activity,train_data))
 data_comp <- bind_rows(test,train) %>% 
   select(subject,activity,contains(".mean."),contains(".std.")) %>%
   arrange(subject,activity)
-
 
 ##gather X,Y,Z values
